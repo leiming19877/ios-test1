696bcd7d048f17e8ef3fb2da35482bb3fd0ef5c6
leming <leiming@leimings-MacBook-Pro.local>
2017-03-27 14:46:35 +0800

    1、项目初始化代码。

---
+++ iso-test1/iso-test1Tests/iso_test1Tests.swift
@@ -0,0 +1,36 @@
+//
+//  iso_test1Tests.swift
+//  iso-test1Tests
+//
+//  Created by leming on 2017/3/27.
+//  Copyright © 2017年 leming. All rights reserved.
+//
+
+import XCTest
+@testable import iso_test1
+
+class iso_test1Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
